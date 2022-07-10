fe3e0aae5a9a759602f34256195991ec498cb9c7
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-07-10 17:17:36 +0000

    avance fin de semana

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,13 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 #your code go here:
+
+new_list = []
+
+for i in my_list:
+    if type(i) == list:
+        new_list.append(i)
+    if type(i) == dict:
+        new_list.append(i)
+
+print(new_list)
--- exercises/05-Sum_all_items/app.py
+++ exercises/05-Sum_all_items/app.py
@@ -4,8 +4,9 @@ my_sample_list = [3423,5,4,47889,654,8,867543,23,48,5345,234,6,78,54,23,67,3,6,4
 def sum_all_values(items):
 
     total= 0
-    #The magic happens here:
-   
+    for item in items:
+        total = total + item
 
     return total
+
 print(sum_all_values(my_sample_list))
